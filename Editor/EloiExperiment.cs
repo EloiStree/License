3648aa941e5dfe914e0eb21ce3738dc918daf30d
Eloi Stree <eloistree@gmail.com>
2020-07-30 21:58:58 +0200

    Save: 07/30/2020 9:58 PM

---
+++ Editor/EloiExperiment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EloiExperiment : MonoBehaviour
+{
+    public const string EDITORMENUSTART = "ꬲ🧰";
+    public const string INFOMENU = "Info on ꬲ🧰";
+   
+    [MenuItem(EDITORMENUSTART + "/"+ INFOMENU + "/💲 Applied License", false, 10000)]
+    static void License()
+    {
+        Application.OpenURL("https://eloistree.page.link/license");
+    }
+
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU , true, 1)]
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/❗ Information", false, 9996)]
+    static void Contact()
+    {
+        Application.OpenURL("https://eloistree.page.link/contact");
+    }
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/🐞 Bug + Issue", false, 9997)]
+    static void Issue()
+    {
+        Application.OpenURL("https://eloistree.page.link/issue");
+    }
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/💬 Discord talk", false, 9998)]
+    static void Discord()
+    {
+        Application.OpenURL("https://eloistree.page.link/discord");
+    }
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/📡 RSS Stay Tuned", false, 9999)]
+    static void RSS()
+    {
+        Application.OpenURL("https://eloistree.page.link/rss");
+    }
+    [MenuItem(EDITORMENUSTART + "/☕ Ko-Fi", false, 10001)]
+    static void KoFi()
+    {
+        Application.OpenURL("https://eloistree.page.link/donation");
+    }
+
+}
---
+++ Runtime/CreditPageBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditPageBehaviour : MonoBehaviour
+{
+    public string m_nextScene;
+    public float m_autoLoadingTime = 5f;
+    private bool m_sceneLoaderCall;
+    public void OpenTargetUrl(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    void Update()
+    {
+        if (m_autoLoadingTime > 0)
+        { 
+        
+        m_autoLoadingTime -= Time.deltaTime;
+        }  
+        else if (!m_sceneLoaderCall) {
+            m_sceneLoaderCall = true;
+
+            if (m_nextScene.Length > 0)
+                SceneManager.LoadScene(m_nextScene);
+            else SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
