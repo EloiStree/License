47d94896f2c8a34ccbe67e816cb273f3ddfdeab6
Éloi Strée <eloistree@gmail.com>
2024-03-28 17:35:47 +0100

    Save: 03/28/2024 5:35 PM

--- Editor/EloiExperiment.cs
+++ Editor/EloiExperiment.cs
@@ -8,43 +8,37 @@ public class EloiExperiment : MonoBehaviour
     public const string EDITORMENUSTART = "ꬲ🧰";
     public const string INFOMENU = "✋ Hi you...";
    
-    [MenuItem(EDITORMENUSTART + "/"+ INFOMENU + "/💲 Applied License", false, 10000)]
-    static void License()
-    {
-        Application.OpenURL("https://eloistree.page.link/license");
-    }
-
+    
     [MenuItem(EDITORMENUSTART + "/" + INFOMENU , true, 1)]
-    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/❗ Information", false, 9996)]
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/❗ GitHub & Linked In ", false, 10000)]
     static void Contact()
     {
-        Application.OpenURL("https://eloistree.page.link/whoami");
+        Application.OpenURL("https://github.com/EloiStree/");
     }
-    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/🐞 Bug + Issue", false, 9997)]
+
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/☕ Donation & License", false, 9996)]
+    static void License()
+    {
+        Application.OpenURL("https://en.tipeee.com/eloilab");
+    }
+
+
+    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/🐞 Report Bug", false, 9997)]
     static void Issue()
     {
-        Application.OpenURL("https://eloistree.page.link/issue");
+        Application.OpenURL("https://github.com/EloiStree/EloiStree/issues");
     }
     [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/💬 Discord talk", false, 9998)]
     static void Discord()
     {
-        Application.OpenURL("https://eloistree.page.link/discord");
+        Application.OpenURL("https://eloistree.github.io/r/discord");
     }
-    [MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/📡 Git News", false, 9999)]
-    static void GitHubNEWS()
-    {
-        Application.OpenURL("https://eloistree.page.link/github");
-    }
-    [MenuItem(EDITORMENUSTART + "/☕ Ko-Fi", false, 10001)]
-    static void KoFi()
-    {
-        Application.OpenURL("https://eloistree.page.link/donation");
-    }
+  
     
     [MenuItem(EDITORMENUSTART + "/🧰 How to use...", false, 10002)]
     static void HowToUse()
     {
-        Application.OpenURL("https://eloistree.page.link/usemyunitytoolboxes");
+        Application.OpenURL("https://github.com/EloiStree/UnityToolbox/discussions/26");
     }
     //[MenuItem(EDITORMENUSTART + "/" + INFOMENU + "/ Yo !", false, 10003)]
     static void WhoAmI()
